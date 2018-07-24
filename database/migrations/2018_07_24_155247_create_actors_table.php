574f01afce5f771663f44c68aee2a8435f7f68fe
tieupham2803 <tieupham2803@gmail.com>
2018-07-24 23:13:14 +0700

    fix convention  + modify migrate

---
+++ database/migrations/2018_07_24_155247_create_actors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('actors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('avarta')->nullable();
+            $table->integer('country_id')->unsigned()->nullable();
+            $table->date('birthday')->nullable();
+            $table->text('infor')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('actors');
+    }
+}
